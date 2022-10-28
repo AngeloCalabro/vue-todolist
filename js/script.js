67420df71e01fb80259f26be76e828305746a6ea
AngeloCalabro <angelo.1991@hotmail.com>
2022-10-28 15:38:52 +0200

    addToDo

--- js/script.js
+++ js/script.js
@@ -1,10 +1,38 @@
-
 const { createApp } = Vue
 
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            newtask: '',
+            hasError: false,
+            item: 0,
+            listToDo: [
+                {
+                    text: 'ToDo1',
+                    done: false
+                },
+                {
+                    text: 'ToDo2',
+                    done: false
+                }
+            ]
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+        addToDo() {
+            if (this.newtask.length >= 3) {
+                this.listToDo[item].text.unshift(this.newtask)
+                this.hasError = false;
+            } else {
+                this.hasError = true;
+            }
+            this.newtask = ''
+        },
+        removeToDo() {
+
         }
     }
 }).mount('#app')
