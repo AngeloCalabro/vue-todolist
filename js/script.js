577df9b7aec11a45a4cb9694b43852bff57551d5
AngeloCalabro <angelo.1991@hotmail.com>
2022-10-28 17:01:24 +0200

    addToDo check

--- js/script.js
+++ js/script.js
@@ -22,15 +22,15 @@ createApp({
     methods: {
         addToDo() {
             if (this.newtask.text.length >= 3) {
-                this.listToDo.text.unshift(this.newtask.text)
+                this.listToDo.unshift(this.newtask)
                 this.hasError = false;
             } else {
                 this.hasError = true;
             }
-            this.newtask.text = '';
+            // this.newtask = '';
         },
         removeToDo(i) {
-            // this.listToDo.splice(i, 1)
+            this.listToDo.splice(i, 1)
         },
         checkToDo() {
             // this.item.done = true
